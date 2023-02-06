5f3a2ecd90a2954b8f1115f0b9087e64c88a694f
F1rstID <wndhdks4536@gmail.com>
2023-02-06 20:03:51 +0900

    :heavy_plus_sign: Add swagger, passport
    API 문서 작성을 위한 Swagger, Login을 위한 passport 추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { HttpExceptionFilter } from './common/filter/http-exception-filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(3000);
 }
 bootstrap();
