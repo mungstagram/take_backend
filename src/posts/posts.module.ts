05d753d07043eee0fe18366d2b95960cd4640eeb
sch7878 <sch8787@naver.com>
2023-02-08 18:27:09 +0900

    ⚡️이미지 및 영상 업로드 여러장 가능하게로 수정

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -4,9 +4,14 @@ import { Module } from '@nestjs/common';
 import { PostsController } from './controllers/posts.controller';
 import { PostsService } from './services/posts.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forFeature([Posts])],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forFeature([Posts]),
+    AuthModule,
+  ],
   controllers: [PostsController],
   providers: [PostsService],
   exports: [PostsService],
--- src/posts/services/posts.service.ts
+++ src/posts/services/posts.service.ts
@@ -3,9 +3,8 @@ import { PostsCreateRequestsDto } from './../dto/postscreate.request.dto';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Posts } from 'src/entities/Posts';
-import { DataSource, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import * as AWS from 'aws-sdk';
-import { PromiseResult } from 'aws-sdk/lib/request';
 import * as path from 'path';
 
 @Injectable()
@@ -13,6 +12,7 @@ export class PostsService {
   private readonly awsS3: AWS.S3;
   public readonly S3_BUCKET_NAME: string;
 
+  //의존성 주입
   constructor(
     @InjectRepository(Posts)
     private readonly postsRepository: Repository<Posts>,
@@ -26,64 +26,46 @@ export class PostsService {
     this.S3_BUCKET_NAME = this.configService.get('AWS_S3_BUCKET_NAME');
   }
 
+  //게시글 작성 서비스
   async createPosts(
     data: PostsCreateRequestsDto,
     folder: string,
-    file: Express.Multer.File,
+    files: Array<Express.Multer.File>,
   ) {
     const { content, hashTags, UserId } = data;
+    let result = '';
 
-    const key = `${folder}/${Date.now()}_${path.basename(
-      file.originalname,
-    )}`.replace(/ /g, '');
+    //file 별로 구분하여 s3에 저장
+    files.forEach((file) => {
+      const key = `${folder}/${Date.now()}_${path.basename(
+        file.originalname,
+      )}`.replace(/ /g, '');
 
-    const s3Object = await this.awsS3
-      .putObject({
-        Bucket: this.S3_BUCKET_NAME,
-        Key: key,
-        Body: file.buffer,
-        ACL: 'public-read',
-        ContentType: file.mimetype,
-      })
-      .promise();
+      this.awsS3
+        .putObject({
+          Bucket: this.S3_BUCKET_NAME,
+          Key: key,
+          Body: file.buffer,
+          ACL: 'public-read',
+          ContentType: file.mimetype,
+        })
+        .promise();
+      const content_url = `https://${this.S3_BUCKET_NAME}.s3.amazonaws.com/${key}`;
 
-    const content_url = `https://${this.S3_BUCKET_NAME}.s3.amazonaws.com/${key}`;
+      result += content_url;
+      result += ',';
+    });
 
+    result = result.slice(0, -1);
+
+    //DB에 내용 데이터와 S3에 저장된 이미지 및 영상 데이터 URL 저장
     const post = await this.postsRepository.save({
       content,
-      content_url,
+      content_url: result,
       hashTags,
       UserId,
     });
 
     return post;
   }
-
-  // async uploadImg(
-  //   folder: string,
-  //   file: Express.Multer.File,
-  // ): Promise<{
-  //   imgurl: string;
-  // }> {
-  //   try {
-  //     const key = `${folder}/${Date.now()}_${path.basename(
-  //       file.originalname,
-  //     )}`.replace(/ /g, '');
-
-  //     const s3Object = await this.awsS3
-  //       .putObject({
-  //         Bucket: this.S3_BUCKET_NAME,
-  //         Key: key,
-  //         Body: file.buffer,
-  //         ACL: 'public-read',
-  //         ContentType: file.mimetype,
-  //       })
-  //       .promise();
-  //     return {
-  //       imgurl: `https://${this.S3_BUCKET_NAME}.s3.amazonaws.com/${key}`,
-  //     };
-  //   } catch (error) {
-  //     throw error;
-  //   }
-  // }
 }
